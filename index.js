d76a873b659e0884399b6a2b421b7e212ee0860c
Yusup <hunzah9@mail.ru>
2023-08-15 14:35:19 +0300

    commit

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ let http = require('http')
 
 let server = http.createServer( (req, res)=>{
     console.log('some request')
+    res.write('aaaaaa')
+    res.end()
+
+
+    switch (url) {
+        case '/': res.write(`<h2>Home<h2/>`)
+            break;
+            case '/tasks': res.write(`<h2>Tasks<h2/>`)
+            break;
+        default: res.write(`<h2>Page Not Found<h2/>`)
+            break;
+    }
+    res.end()
 }  )
+
+
 
 server.listen(7542);
